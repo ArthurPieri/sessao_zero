dcd79e5d720a6aa90264f133b74992f091e7e0d4
arthurpieri <git@arthurpieri.com>
2023-04-07 00:19:20 -0300

    feat: init project

---
+++ src/app.py
@@ -0,0 +1,7 @@
+'''This is the main entry point of the application. It is responsible for
+creating the Flask application and configuring it. It also registers the
+blueprints for the different parts of the application.'''
+
+from flask import Flask
+
+app = Flask(__name__)
