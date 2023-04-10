ecadfc8c2e6ff81ad97ee522aa775b583cf139c0
arthurpieri <services+github@arthurpieri.com>
2023-04-10 11:35:52 -0300

    fix: tables relationship

--- src/db/sql/create_tables/2_create_player.sql
+++ src/db/sql/create_tables/2_create_player.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS player (
     username VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
     admin BOOLEAN,
-    FOREIGN KEY (char_id) REFERENCES player_character (id),
     FOREIGN KEY (guild_id) REFERENCES guild (id)
 );
--- src/db/sql/create_tables/3_create_character.sql
+++ src/db/sql/create_tables/3_create_character.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS player_character (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64) NOT NULL,
-    description VARCHAR(5000) NOT NULL
+    description VARCHAR(5000) NOT NULL,
+    FOREIGN KEY (player_id) REFERENCES player (id),
 );
